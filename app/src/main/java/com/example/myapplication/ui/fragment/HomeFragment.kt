bf2f756d745be8692559e443600a959d9e1db26e
苏木 <1379169921@qq.com>
2024-05-20 11:44:06 +0800

    1212

--- app/src/main/java/com/example/myapplication/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/fragment/HomeFragment.kt
@@ -6,13 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.utils.SpUtils
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,11 +23,9 @@ class HomeFragment : Fragment() {
 //            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.homeText.text = SpUtils.getValue("loginUserName")
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+
         return binding.root
     }
 
